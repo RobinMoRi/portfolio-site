01cfcd6df5b707bf4dc307a4645dc213c5f77f6f
Robin Moreno Rinding <robin.moreno.rinding@gmail.com>
2024-02-21 12:23:00 +0100

    Revert "Merge pull request #15 from RobinMoRi/bug/10-weird-footer"
    This reverts commit 6ca3ff240913c647a10a4967befc93cfb68a4bc7, reversing
    changes made to 2825df0c826021f103a614a39b0a1e4f46f644ac.

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -35,54 +35,61 @@ async function copyToClipboard(text: string) {
   <div id="contacts">
     <Toolbar style="border: none">
       <template #start>
-        <div
-          class="flex gap-4 justify-content-center align-items-center ml-4 my-4 md:my-2"
-        >
-          <Button
-            rounded
-            v-tooltip.top="{ value: 'Linkedin', autoHide: false }"
-            size="small"
-            icon="pi pi-linkedin"
-            @click="openUrl('https://www.linkedin.com/in/romori/')"
-            outlined
-          />
-          <Button
-            rounded
-            v-tooltip.top="{ value: 'Github', autoHide: false }"
-            size="small"
-            icon="pi pi-github"
-            @click="openUrl('https://github.com/RobinMoRi')"
-            outlined
-          />
-          <Button
-            rounded
-            v-tooltip.top="{ value: 'Email', autoHide: false }"
-            size="small"
-            icon="pi pi-envelope"
-            @click="openUrl('mailto:robin.moreno.rinding@gmail.com')"
-            outlined
-          />
-          <Button
-            rounded
-            v-tooltip.top="{ value: 'Copy number', autoHide: false }"
-            size="small"
-            icon="pi pi-phone"
-            @click="copyToClipboard('+46737514695')"
-            outlined
-          />
+        <div>
+          <div
+            class="grid gap-4 justify-content-center align-items-center ml-4"
+          >
+            <Button
+              rounded
+              v-tooltip.top="{ value: 'Linkedin', autoHide: false }"
+              size="small"
+              icon="pi pi-linkedin"
+              @click="openUrl('https://www.linkedin.com/in/romori/')"
+              outlined
+            />
+            <Button
+              rounded
+              v-tooltip.top="{ value: 'Github', autoHide: false }"
+              size="small"
+              icon="pi pi-github"
+              @click="openUrl('https://github.com/RobinMoRi')"
+              outlined
+            />
+            <Button
+              rounded
+              v-tooltip.top="{ value: 'Email', autoHide: false }"
+              size="small"
+              icon="pi pi-envelope"
+              @click="openUrl('mailto:robin.moreno.rinding@gmail.com')"
+              outlined
+            />
+            <Button
+              rounded
+              v-tooltip.top="{ value: 'Copy number', autoHide: false }"
+              size="small"
+              icon="pi pi-phone"
+              @click="copyToClipboard('+46737514695')"
+              outlined
+            />
+          </div>
         </div>
       </template>
       <template #end>
-        <div class="text-xs text-200 mr-1">
-          This page is built by Robin Moreno Rinding using Vue3, PrimeVue,
-          PrimeFlex. Portfolio is integrated with
+        <div class="flex flex-row">
+          <div class="text-xs text-200 mr-1">
+            This page is built by Robin Moreno Rinding using Vue3, PrimeVue,
+            PrimeFlex. Portfolio is integrated with
+          </div>
           <a
             class="text-xs text-200 mr-1"
             href="https://docs.github.com/en/rest?apiVersion=2022-11-28"
             target="_blank"
           >
             Github API.</a
-          >Illustrations are done in Figma.
+          >
+          <div class="text-xs text-200 mr-1">
+            Illustrations are done in Figma.
+          </div>
         </div>
       </template>
     </Toolbar>
