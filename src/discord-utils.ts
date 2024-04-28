e57535a838c965d885a2169b7e1d7f7769929c18
Robin Moreno Rinding <robin.moreno.rinding@gmail.com>
2024-04-28 14:31:49 +0200

    add comment

--- src/discord-utils.ts
+++ src/discord-utils.ts
@@ -1,5 +1,3 @@
-// TODO: Move to environment variables
-// const DISCORD_PROXY_HOST = "http://localhost:8080";
 const DISCORD_PROXY_HOST = import.meta.env.VITE_DISCORD_PROXY_HOST;
 
 export interface CreateThreadResponse {
