122a314ce6d50a3e76028a2d28d39b029ef9d38f
Robin Moreno Rinding <robin.moreno.rinding@gmail.com>
2024-08-22 21:05:54 +0200

    moved some env vars etc

--- api/main.py
+++ api/main.py
@@ -2,8 +2,6 @@
 Proxy for discord - used to go around discords stupid cors policy
 """
 
-from typing import Union
-
 from fastapi import FastAPI
 import requests
 from fastapi.middleware.cors import CORSMiddleware
@@ -29,10 +27,18 @@ app.add_middleware(
 CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
 BOT_ID = os.getenv("DISCORD_BOT_ID")
 AUTH_TOKEN = os.getenv("DISCORD_AUTH_TOKEN")
+GITHUB_API_TOKEN = os.getenv("VITE_GITHUB_API_TOKEN")
 HEADERS = {
     "Authorization": f"Bot {AUTH_TOKEN}",
     "Accept": "*/*",
     "Content-Type": "application/json",
+}
+
+
+GITHUB_API_HEADERS = {
+    "accept": "application/vnd.github+json",
+    "authorization": f"Bearer {GITHUB_API_TOKEN}",
+    "X-GitHub-Api-Version": "2022-11-28",
 }
 
 
@@ -77,9 +83,29 @@ def create_thread_message(message: str, thread_id: str, meta=False):
 
 
 @app.get("/getThreadMessages")
-def create_thread_message(thread_id: str):
+def get_thread_message(thread_id: str):
     res = requests.get(
         url=f"https://discord.com/api/v10/channels/{thread_id}/messages",
+        headers=HEADERS,
+    )
+    res.raise_for_status()
+    return res.json()
+
+
+@app.get("/getGithubRepos")
+def get_gh_repos():
+    res = requests.get(
+        url="https://api.github.com/user/repos",
+        headers=GITHUB_API_HEADERS,
+    )
+    res.raise_for_status()
+    return res.json()
+
+
+@app.get("/getGithubLanguages")
+def get_gh_languages(url: str):
+    res = requests.get(
+        url=url,
         headers=HEADERS,
     )
     res.raise_for_status()
