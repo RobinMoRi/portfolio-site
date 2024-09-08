7abd47d187183efd34d5cdcae45369febcce0fb7
Robin Moreno Rinding <robin.moreno.rinding@gmail.com>
2024-09-08 15:28:13 +0200

    add prismicio

--- portfolio-frontend/app/components/sections/AboutMe.tsx
+++ portfolio-frontend/app/components/sections/AboutMe.tsx
@@ -1,14 +1,60 @@
-"use client";
 import { Section } from "@/types/navbar";
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
 
 const AboutMe = () => {
   return (
-    <SectionWrapper id={Section.AboutMe} className="h-full">
-      <div className="h-full">About Me</div>
+    <SectionWrapper id={Section.AboutMe} className="h-full" title="About Me">
+      <AboutMeContent />
+      <div className="md:max-w-prose">
+        <p>
+          With a master's degree in mechatronics and robotics from the Royal
+          Institute of Technology, I come most recently from Morpheus Tribe,
+          where I work as a fullstack developer.
+        </p>
+        <p>
+          I have over 4 years of hands-on experience working with a diverse
+          range of technologies, encompassing web development, business process
+          automation, data analytics, and test development.
+        </p>
+        <p>
+          What truly sets me apart is my unwavering passion for fullstack
+          development. I am not just content with my existing knowledge; I am
+          always eager to learn more and delve deeper into this ever-evolving
+          field.
+        </p>
+        <p>
+          Outside of my professional life, I find great joy in running,
+          especially over long distances. In addition to this, I have a deep
+          love for hitting the gym, traveling and exploring everything related
+          to food.
+        </p>
+      </div>
     </SectionWrapper>
   );
 };
 
 export default AboutMe;
+
+import { SliceZone } from "@prismicio/react";
+import { Metadata } from "next";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export async function AboutMeContent() {
+  const client = createClient();
+  const page = await client.getSingle("aboutme");
+
+  return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("aboutme");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
+}
--- portfolio-frontend/app/components/sections/SectionWrapper.tsx
+++ portfolio-frontend/app/components/sections/SectionWrapper.tsx
@@ -1,23 +1,30 @@
-import useNavbarHeight from "@/app/hooks/useNavbarHeight";
 import { Section } from "@/types/navbar";
 import React from "react";
 
 interface SectionWrapperProps {
   children: React.ReactNode;
   className: string;
+  title?: string;
   id: string;
 }
-const SectionWrapper = ({ children, className, id }: SectionWrapperProps) => {
-  const navbarHeight = useNavbarHeight();
+const SectionWrapper = ({
+  children,
+  className,
+  id,
+  title,
+}: SectionWrapperProps) => {
   return (
     <div
       id={id}
       className={`${className} px-2 md:px-20 lg:px-52`}
       style={{
-        paddingTop: id === Section.Init ? navbarHeight : 0,
+        paddingTop: id === Section.Init ? "72px" : 0,
       }}
     >
-      {children}
+      <div className="pt-10 max-h-full overflow-scroll">
+        {title ? <div className="text-5xl pb-6">{title}</div> : null}
+        {children}
+      </div>
     </div>
   );
 };
