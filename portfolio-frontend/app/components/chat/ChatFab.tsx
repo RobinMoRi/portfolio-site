a24df289845e85063233d56978bcbcce6066d6b7
Robin Moreno Rinding <robin.moreno.rinding@gmail.com>
2024-09-17 00:01:21 +0200

    Add bounce anim

--- portfolio-frontend/app/components/chat/ChatFab.tsx
+++ portfolio-frontend/app/components/chat/ChatFab.tsx
@@ -1,21 +1,25 @@
 "use client";
 
 import { MessageSquare } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import ChatModal from "./ChatModal";
 
 const ChatFab = () => {
+  const [openedOnce, setOpenedOnce] = useState(false);
   const trigger = (
     <div
       className="z-50 w-10 h-10 p-2 bg-navbar rounded-lg"
-      onClick={() => console.log("anik ummak")}
+      onClick={() => setOpenedOnce(true)}
     >
       <MessageSquare />
     </div>
   );
+  const content = <div>KUSS UCHTA</div>;
   return (
-    <div className="fixed bottom-4 right-4 sm:bottom-8 sm:right-8 z-50">
-      <ChatModal trigger={trigger} content={"anik ummak kahpe"} />
+    <div
+      className={`fixed bottom-4 right-4 sm:bottom-8 sm:right-8 z-50 ${!openedOnce ? "animate-bounce" : ""}`}
+    >
+      <ChatModal trigger={trigger} content={content} />
     </div>
   );
 };
